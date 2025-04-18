a7441ccfd9d258582aa7f48b8240ac17c1bac8c1
hyunwoo <kshsim98@naver.com>
2025-04-18 17:20:41 +0900

    requestMapping으로 변경

--- src/main/java/HS_hrs/user_service/Controller/HomeController.java
+++ src/main/java/HS_hrs/user_service/Controller/HomeController.java
@@ -1,12 +1,14 @@
 package HS_hrs.user_service.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class HomeController {
 
-    @GetMapping("/user")
+    @GetMapping
     public String home() {
         return "유저 서비스입니다.";
     }
