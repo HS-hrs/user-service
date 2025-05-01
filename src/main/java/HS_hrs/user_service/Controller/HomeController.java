d7f403e9f172f7c4657deaa4d0d59f3573a68dda
hyunwoo <kshsim98@naver.com>
2025-05-01 16:06:52 +0900

    랜덤포트 사용

--- src/main/java/HS_hrs/user_service/Controller/HomeController.java
+++ src/main/java/HS_hrs/user_service/Controller/HomeController.java
@@ -1,16 +1,34 @@
 package HS_hrs.user_service.Controller;
 
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class HomeController {
+
+  Environment env;
+
+  public HomeController(Environment env) {
+    this.env = env;
+  }
 
     @GetMapping
     public String home() {
         return "유저 서비스입니다.";
     }
 
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+      log.info("Server port={}", request.getServerPort());
+      return String.format("Check from Server running at port %s", env.getProperty("local.server.port"));
+
+    }
 }
